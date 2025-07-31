48d8491f41de43afc9bc09c0928dc4d2f7cb9291
coolmp08 <coolmp08@gmail.com>
2025-07-31 12:08:33 +0530

    initial commit

---
+++ src/test/java/io/devsense/openapigenerator_demo/OpenapigeneratorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.devsense.openapigenerator_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapigeneratorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
